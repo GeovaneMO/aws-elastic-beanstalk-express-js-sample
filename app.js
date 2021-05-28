21af5df2588bfb7e87daeb712820707749f36420
GeovaneMO <jovane.geovane@hotmail.com>
2021-05-28 10:39:47 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('vamo ver oque eu vou aprender aqui'));
+app.get('/', (req, res) => res.send('mudança pro modulo 5, Geovane é lindo'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
